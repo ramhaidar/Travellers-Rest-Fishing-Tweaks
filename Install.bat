25dd3ca3f2a52c5252ac58e17db455ddec186bc9
Doug <doug.stalker@gmail.com>
2024-07-27 16:17:08 +1000

    1. renamed things 2. made it compile again after breaking things by renaming them

---
+++ Install.bat
@@ -0,0 +1,1 @@
+copy "D:\TR\AddOneGold\bin\Debug\netstandard2.1\net.nep.bepinex.trtest3.dll"  "C:\Games\Steam\steamapps\common\Travellers Rest\Windows\BepInEx\plugins\net.nep.bepinex.addonegold.dll"
---
+++ NepEasyFishing.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using UnityEngine;
+
+namespace NepEasyFishing
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+
+        private readonly ConfigEntry<bool> _FishBarQuickProgress;
+        private readonly ConfigEntry<bool> _FishBarNoDecrease;
+        private readonly ConfigEntry<bool> _FishBarQuickBites;
+        public Plugin()
+        {
+
+            _FishBarQuickProgress = Config.Bind("General", "Quick Progress", true, "Fishing progress fills quickly when fish is clicked on");
+            _FishBarNoDecrease = Config.Bind("General", "No Bar Decrease", true, "Fishing progress does not lower");
+            _FishBarQuickBites = Config.Bind("General", "Quick Bites", true, "reduced time before bites");
+        }
+
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
